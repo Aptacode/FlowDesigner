522431a70fa6bcf7ad965c45f2c78b7bdf1bf13a
Timothy Jones <t.jones@timmoth.com>
2020-12-02 17:37:55 +0000

    Moved demo configuration into FlowDesigner.Demo project

--- FlowDesigner.Blazor/Components/Diagram.razor.cs
+++ FlowDesigner.Blazor/Components/Diagram.razor.cs
@@ -10,6 +10,7 @@ namespace FlowDesigner.Blazor.Components
 {
     public class DiagramBase : ComponentBase
     {
+        [Parameter]
         public DesignerViewModel Designer { get; set; }
 
         protected ElementReference diagram;  // set by the @ref attribute
@@ -27,20 +28,6 @@ namespace FlowDesigner.Blazor.Components
 
         protected override async Task OnInitializedAsync()
         {
-            Designer = new DesignerViewModel();
-            Designer.CreateItem.Execute(("State 1", new Vector2(8, 8), new Vector2(8, 4)));
-            Designer.CreateItem.Execute(("State 2", new Vector2(8, 16), new Vector2(8, 4)));
-            Designer.CreateItem.Execute(("State 3", new Vector2(25, 30), new Vector2(8, 4)));
-            Designer.CreateItem.Execute(("State 4", new Vector2(10, 40), new Vector2(8, 4)));
-            Designer.CreateItem.Execute(("State 5", new Vector2(10, 50), new Vector2(8, 4)));
-            Designer.CreateItem.Execute(("State 6", new Vector2(25, 60), new Vector2(8, 4)));
-
-            var items = Designer.Items.ToList();
-            Designer.Connect.Execute((items[0], items[1]));
-            Designer.Connect.Execute((items[0], items[2]));
-            Designer.Connect.Execute((items[0], items[3]));
-            Designer.Connect.Execute((items[0], items[4]));
-
             Designer.RedrawConnections();
 
             await base.OnInitializedAsync();
